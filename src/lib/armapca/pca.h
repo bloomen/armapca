03ac043c4f93257325471496b8fda2254c71aebf
Christian Blume <chr.blume@gmail.com>
2016-03-12 19:29:22 -0700

    move impl to cpp file

--- src/lib/armapca/pca.h
+++ src/lib/armapca/pca.h
@@ -2,9 +2,48 @@
 #include <armadillo>
 #include <algorithm>
 #include <random>
+#include <vector>
 
 
 namespace armapca {
+
+
+template<typename T>
+struct pca_result {
+  pca_result();
+  arma::Mat<T> eigenvectors;
+  arma::Col<T> eigenvalues;
+  T energy;
+};
+
+
+template<typename T>
+armapca::pca_result<T> pca(const arma::Mat<T>& data,
+                           bool compute_eigenvectors = true);
+
+
+template<typename T>
+std::vector<armapca::pca_result<T>> pca_bootstrap(const arma::Mat<T>& data,
+                                              bool compute_eigenvectors = true,
+                                              std::size_t n_bootstraps = 100,
+                                              std::size_t random_seed = 1);
+
+
+template<typename T>
+T check_eigenvectors_orthogonal(const armapca::pca_result<T>& result);
+
+
+template<typename T>
+T check_projection_accurate(const arma::Mat<T>& data,
+                            const armapca::pca_result<T>& result);
+
+
+template<typename T>
+void remove_mean(arma::Mat<T>* matrix);
+
+
+template<typename T>
+void normalize(arma::Mat<T>* matrix);
 
 
 template<typename T>
@@ -16,146 +55,23 @@ arma::Mat<T> shuffled_matrix(const arma::Mat<T>& matrix, std::mt19937* gen);
 
 
 template<typename T>
-void enforce_positive_sign_by_column(arma::Mat<T>* matrix) {
-  for (std::size_t i=0; i < matrix->n_cols; ++i) {
-    const arma::uvec indices = arma::sort_index(matrix->col(i), 1);
-    const auto min = matrix->col(i)(indices(0));
-    const auto max = matrix->col(i)(indices(indices.n_elem - 1));
-    bool change_sign = false;
-    if (std::abs(max) >= std::abs(min)) {
-      if (max < 0) change_sign = true;
-    } else {
-      if (min < 0) change_sign = true;
-    }
-    if (change_sign) matrix->col(i) *= T{-1};
-  }
-}
-
-
-template<typename T, typename Functor>
-arma::Row<T> column_apply(const arma::Mat<T>& matrix, Functor functor) {
-  arma::Row<T> result(matrix.n_cols);
-  std::size_t index = 0;
-  std::generate(result.begin(), result.end(), [&matrix, &index] {
-    return Functor(matrix.col(index++));
-  });
-  return result;
-}
+void enforce_positive_sign_by_column(arma::Mat<T>* matrix);
 
 
 template<typename T>
-arma::Row<T> column_mean(const arma::Mat<T>& matrix) {
-  return column_apply(matrix, arma::mean<T>);
-}
+arma::Row<T> column_mean(const arma::Mat<T>& matrix);
 
 
 template<typename T>
-arma::Row<T> column_stddev(const arma::Mat<T>& matrix) {
-  return column_apply(matrix, arma::stddev<T>);
-}
-
-
-template<typename T, typename Vector>
-void remove_by_column(arma::Mat<T>* matrix, const Vector& vector) {
-  for (std::size_t i=0; i < matrix->n_cols; ++i)
-    matrix->col(i) -= vector(i);
-}
-
-
-template<typename T, typename Vector>
-void divide_by_column(arma::Mat<T>* matrix, const Vector& vector) {
-  for (std::size_t i=0; i < matrix->n_cols; ++i)
-    matrix->col(i) *= T {1} / vector(i);
-}
+arma::Row<T> column_stddev(const arma::Mat<T>& matrix);
 
 
 template<typename T>
-void remove_mean(arma::Mat<T>* matrix) {
-  const auto mean = armapca::column_mean(matrix);
-  armapca::remove_by_column(matrix, mean);
-}
+void remove_by_column(arma::Mat<T>* matrix, const arma::Row<T>& vector);
 
 
 template<typename T>
-void normalize(arma::Mat<T>* matrix) {
-  const auto stddev = armapca::column_stddev(matrix);
-  armapca::divide_by_column(matrix, stddev);
-}
-
-
-template<typename T>
-struct pca_result {
-  arma::Mat<T> eigenvectors;
-  arma::Col<T> eigenvalues;
-  T energy;
-};
-
-
-template<typename T>
-armapca::pca_result<T> pca(const arma::Mat<T>& data,
-                           bool compute_eigenvectors = true) {
-  const auto n_vars = data.n_cols;
-  arma::Mat<T> eigvec;
-  if (compute_eigenvectors)
-    eigvec.set_size(n_vars, n_vars);
-  arma::Col<T> eigval(n_vars);
-
-  const auto cov_mat = armapca::covariance_matrix(data);
-  if (compute_eigenvectors)
-    arma::eig_sym(eigval, eigvec, cov_mat);
-  else
-    arma::eig_sym(eigval, cov_mat);
-  const arma::uvec indices = arma::sort_index(eigval, 1);
-
-  armapca::pca_result<T> result = {arma::Mat<T>(n_vars, n_vars),
-      arma::Col<T>(n_vars), 0};
-  for (std::size_t i=0; i < n_vars; ++i) {
-    result.eigenvalues(i) = eigval(indices(i));
-  }
-
-  if (compute_eigenvectors) {
-    for (std::size_t i=0; i < n_vars; ++i) {
-      result.eigenvectors.col(i) = eigvec.col(indices(i));
-    }
-    armapca::enforce_positive_sign_by_column(&result.eigenvectors);
-  }
-
-  result.energy = arma::sum(result.eigenvalues);
-  result.eigenvalues *= T {1} / result.energy;
-
-  return result;
-}
-
-
-template<typename T>
-std::vector<armapca::pca_result<T>> pca_bootstrap(
-    const arma::Mat<T>& data,
-    std::size_t n_bootstraps = 100,
-    bool compute_eigenvectors = true,
-    std::size_t random_seed = 1) {
-  std::vector<armapca::pca_result<T>> result(n_bootstraps);
-  std::mt19937 gen{random_seed};
-  for (std::size_t i=0; i < n_bootstraps; ++i) {
-    const auto shuffled = armapca::shuffled_matrix(data, &gen);
-    result[i] = armapca::pca(shuffled, compute_eigenvectors);
-  }
-  return result;
-}
-
-
-template<typename T>
-T check_eigenvectors_orthogonal(const armapca::pca_result<T>& result) {
-  return std::abs(arma::det(result.eigenvectors));
-}
-
-
-template<typename T>
-T check_projection_accurate(const arma::Mat<T>& data,
-                            const armapca::pca_result<T>& result) {
-  const arma::Mat<double> diff = ((data * result.eigenvectors) *
-      arma::trans(result.eigenvectors)) - data;
-  return T {1} - arma::sum(arma::sum(arma::abs(diff) )) / diff.n_elem;
-}
+void divide_by_column(arma::Mat<T>* matrix, const arma::Row<T>& vector);
 
 
 }  // namespace armapca
--- src/lib/pca.cpp
+++ src/lib/pca.cpp
@@ -38,4 +38,181 @@ template arma::Mat<double> shuffled_matrix(
     const arma::Mat<double>&, std::mt19937*);
 
 
+template<typename T>
+void enforce_positive_sign_by_column(arma::Mat<T>* matrix) {
+  for (std::size_t i=0; i < matrix->n_cols; ++i) {
+    const arma::uvec indices = arma::sort_index(matrix->col(i), 1);
+    const auto min = matrix->col(i)(indices(0));
+    const auto max = matrix->col(i)(indices(indices.n_elem - 1));
+    bool change_sign = false;
+    if (std::abs(max) >= std::abs(min)) {
+      if (max < 0) change_sign = true;
+    } else {
+      if (min < 0) change_sign = true;
+    }
+    if (change_sign) matrix->col(i) *= T{-1};
+  }
+}
+template void enforce_positive_sign_by_column(arma::Mat<float>*);
+template void enforce_positive_sign_by_column(arma::Mat<double>*);
+
+
+template<typename T, typename Functor>
+arma::Row<T> column_apply(const arma::Mat<T>& matrix, Functor functor) {
+  arma::Row<T> result(matrix.n_cols);
+  std::size_t index = 0;
+  std::generate(result.begin(), result.end(), [&matrix, &index, &functor] {
+    return functor(matrix.col(index++));
+  });
+  return result;
+}
+
+
+template<typename T>
+arma::Row<T> column_mean(const arma::Mat<T>& matrix) {
+  return column_apply(matrix, [](const arma::subview_col<T>& v)
+                              { return arma::mean(v); });
+}
+template arma::Row<float> column_mean(const arma::Mat<float>&);
+template arma::Row<double> column_mean(const arma::Mat<double>&);
+
+
+template<typename T>
+arma::Row<T> column_stddev(const arma::Mat<T>& matrix) {
+  return column_apply(matrix, [](const arma::subview_col<T>& v)
+                              { return arma::stddev(v); });
+}
+template arma::Row<float> column_stddev(const arma::Mat<float>&);
+template arma::Row<double> column_stddev(const arma::Mat<double>&);
+
+
+template<typename T>
+void remove_by_column(arma::Mat<T>* matrix, const arma::Row<T>& vector) {
+  for (std::size_t i=0; i < matrix->n_cols; ++i)
+    matrix->col(i) -= vector(i);
+}
+template void remove_by_column(arma::Mat<float>*, const arma::Row<float>&);
+template void remove_by_column(arma::Mat<double>*, const arma::Row<double>&);
+
+
+template<typename T>
+void divide_by_column(arma::Mat<T>* matrix, const arma::Row<T>& vector) {
+  for (std::size_t i=0; i < matrix->n_cols; ++i)
+    matrix->col(i) *= T {1} / vector(i);
+}
+template void divide_by_column(arma::Mat<float>*, const arma::Row<float>&);
+template void divide_by_column(arma::Mat<double>*, const arma::Row<double>&);
+
+
+template<typename T>
+void remove_mean(arma::Mat<T>* matrix) {
+  const auto mean = armapca::column_mean(*matrix);
+  armapca::remove_by_column(matrix, mean);
+}
+template void remove_mean(arma::Mat<float>* matrix);
+template void remove_mean(arma::Mat<double>* matrix);
+
+
+template<typename T>
+void normalize(arma::Mat<T>* matrix) {
+  const auto stddev = armapca::column_stddev(*matrix);
+  armapca::divide_by_column(matrix, stddev);
+}
+template void normalize(arma::Mat<float>* matrix);
+template void normalize(arma::Mat<double>* matrix);
+
+
+template<typename T>
+pca_result<T>::pca_result()
+: eigenvectors{}, eigenvalues{}, energy{} {}
+
+
+template<typename T>
+armapca::pca_result<T> pca(const arma::Mat<T>& data,
+                           bool compute_eigenvectors) {
+  EXPECTS(data.n_rows >= 2);
+  EXPECTS(data.n_cols >= 1);
+  const auto n_vars = data.n_cols;
+  armapca::pca_result<T> result;
+  if (compute_eigenvectors)
+    result.eigenvectors.set_size(n_vars, n_vars);
+  result.eigenvalues.set_size(n_vars);
+
+  const auto cov_mat = armapca::covariance_matrix(data);
+
+  if (compute_eigenvectors) {
+    arma::eig_sym(result.eigenvalues, result.eigenvectors, cov_mat);
+  } else {
+    arma::eig_sym(result.eigenvalues, cov_mat);
+  }
+
+  const arma::uvec indices = arma::sort_index(result.eigenvalues, 1);
+
+  for (std::size_t i=0; i < n_vars; ++i) {
+    result.eigenvalues(i) = result.eigenvalues(indices(i));
+  }
+
+  if (compute_eigenvectors) {
+    for (std::size_t i=0; i < n_vars; ++i) {
+      result.eigenvectors.col(i) = result.eigenvectors.col(indices(i));
+    }
+    armapca::enforce_positive_sign_by_column(&result.eigenvectors);
+  }
+
+  result.energy = arma::sum(result.eigenvalues);
+  result.eigenvalues *= T {1} / result.energy;
+
+  return result;
+}
+template armapca::pca_result<float> pca(const arma::Mat<float>&, bool);
+template armapca::pca_result<double> pca(const arma::Mat<double>&, bool);
+
+
+template<typename T>
+std::vector<armapca::pca_result<T>> pca_bootstrap(const arma::Mat<T>& data,
+                                                  bool compute_eigenvectors,
+                                                  std::size_t n_bootstraps,
+                                                  std::size_t random_seed) {
+  EXPECTS(data.n_rows >= 2);
+  EXPECTS(data.n_cols >= 1);
+  std::vector<armapca::pca_result<T>> result(n_bootstraps);
+  std::mt19937 gen{random_seed};
+  for (std::size_t i=0; i < n_bootstraps; ++i) {
+    const auto shuffled = armapca::shuffled_matrix(data, &gen);
+    result[i] = armapca::pca(shuffled, compute_eigenvectors);
+  }
+  return result;
+}
+template std::vector<armapca::pca_result<float>> pca_bootstrap(
+    const arma::Mat<float>&, bool, std::size_t, std::size_t);
+template std::vector<armapca::pca_result<double>> pca_bootstrap(
+    const arma::Mat<double>&, bool, std::size_t, std::size_t);
+
+
+template<typename T>
+T check_eigenvectors_orthogonal(const armapca::pca_result<T>& result) {
+  return std::abs(arma::det(result.eigenvectors));
+}
+template float check_eigenvectors_orthogonal(
+    const armapca::pca_result<float>&);
+template double check_eigenvectors_orthogonal(
+    const armapca::pca_result<double>&);
+
+
+template<typename T>
+T check_projection_accurate(const arma::Mat<T>& data,
+                            const armapca::pca_result<T>& result) {
+  EXPECTS(data.n_rows >= 2);
+  EXPECTS(data.n_cols >= 1);
+  EXPECTS(result.eigenvectors.n_cols >= 1);
+  const arma::Mat<T> diff = ((data * result.eigenvectors) *
+      arma::trans(result.eigenvectors)) - data;
+  return T {1} - arma::sum(arma::sum(arma::abs(diff) )) / diff.n_elem;
+}
+template float check_projection_accurate(const arma::Mat<float>&,
+                                         const armapca::pca_result<float>&);
+template double check_projection_accurate(const arma::Mat<double>&,
+                                          const armapca::pca_result<double>&);
+
+
 }  // namespace armapca
